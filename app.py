954c1dd683f62f0f7fd5d1b10dcc009f8d4cbfe7
fumiya <156275318+fumiya95@users.noreply.github.com>
2025-02-03 16:49:35 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -13,3 +13,9 @@ if __name__ == "__main__":
 # 数字範囲や個数の定義
 LOTTO7_RANGE = range(1, 38)  # 1～37
 LOTTO7_COUNT = 7
+
+import random
+
+def generate_random_lotto7():
+    # 1～37の中から重複しない7つの数字をランダムに選ぶ
+    return sorted(random.sample(LOTTO7_RANGE, LOTTO7_COUNT))
